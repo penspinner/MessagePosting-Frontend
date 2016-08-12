9464815a76a2c04e833c66be91d822f2b71d6c09
Penspinner <Stvnliao@yahoo.com>
2016-08-12 01:01:02 -0400

    Display messages posted to the textarea as an unordered list.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,6 +3,14 @@ export class MainController {
         'ngInject';
 
         this.$http = $http;
+        this.getMessages();
+    }
+
+    getMessages() {
+        var vm = this;
+        this.$http.get('http://localhost:5000/api/message').then(function(result) {
+            vm.messages = result.data;
+        });
     }
 
     postMessage() {
