84d47a183744063da829aca52f36e6bed24dc017
Penspinner <Stvnliao@yahoo.com>
2016-08-12 02:05:25 -0400

    Created functionality for the registration submit.

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -1,3 +1,11 @@
 export class AuthController {
+    constructor($auth) {
+        'ngInject';
 
+        this.$auth = $auth;
+    }
+
+    register() {
+        this.$auth.signup({email: 'test@test.com'});
+    }
 }
